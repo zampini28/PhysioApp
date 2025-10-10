bba91a75a395ac52beda2f30e58c7852436cae5e
zampini28 <caio.zampini@gmail.com>
2025-10-10 19:01:09 -0300

    feat(startup): add native splash initialization
    Implements manual control over the native splash screen's lifecycle by calling `FlutterNativeSplash.preserve()` when the app starts.
    
    This prevents the splash screen from disappearing prematurely, allowing for essential asynchronous initialization tasks (e.g., loading data, authenticating) to complete. This ensures a smooth transition to the first screen without any visual glitches like a white flash.
    
    The splash screen is intended to be removed via `FlutterNativeSplash.remove()` once initialization is finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:physioapp/homepage.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // use FlutterNativeSplash.remove(); to remove the splash screen after initialization
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
